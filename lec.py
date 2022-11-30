cfcf96603c7897615f1b1047845d026e372a03dc
ilya <tojlctbly@gmail.com>
2022-11-30 18:38:26 +0300

    seminarPy sozdanie

---
+++ lec.py
@@ -0,0 +1,26 @@
+ # тип данных  и переменная
+ # int, float, boolean, str, list, none
+
+# value = None
+# a=123
+# b=1.23
+# print(type(a))
+# print(type(b))
+# print(a)
+# print(b)
+# value = 1234
+# print(value)
+# print(type(value))
+
+# s = "slkfdjhgsdfkljg"
+# print(s)
+# print(a,b,s)
+# print(a,"-",b,"-",s)
+# print("{1} - {2} - {0}".format(a,b,s))
+# print(f"{a} - {b} - {s}")
+
+# f= True
+# print(f)
+
+# list = [1,2, "hello"]
+# print(lis t)
