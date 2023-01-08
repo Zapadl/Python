828ac146fab1f4c691a1e67732a5ba69cdc91a27
ilya <tojlctbly@gmail.com>
2023-01-08 23:19:49 +0300

    Dz- парсер

--- seminar/Les-9/dz-9/Parser.py
+++ seminar/Les-9/dz-9/Parser.py
@@ -1,7 +1,5 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import numpy as np
-
 
 URL_TEMPLATE = "https://mixnews.lv/yumor"
 FILE_NAME = "test.txt"
@@ -23,17 +21,6 @@ def parse_post_title(url = URL_TEMPLATE):
     for name in post_title:
         result_list['h4'].append(name.text)
     return result_list
-
-# def parse_url(url = URL_TEMPLATE):
-#     result_list = {'href': [][]}
-#     r = requests.get(url)
-#     soup = bs(r.text, "html.parser")
-#     url_post = soup.find_all("div",class_="post-item-normal one-posts")
-#     post_title = soup.find_all("h4")
-#     for name in url_post:
-#         result_list['href'].append(name.a['href'] )
-#         result_list['href'].append(name.text)
-#     return result_list
 
 print(parse_url())
 print(parse_post_title())
