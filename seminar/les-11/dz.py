0046110b8e2950690b262ddb5489a80aec7f3549
ilya <tojlctbly@gmail.com>
2023-01-18 23:25:43 +0300

    dz-11

---
+++ seminar/les-11/dz.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+# Создать 2 массива NumPy
+# a = np.random.sample((3,5))
+# print(a)
+
+# Один из случайных чисел, второй - использую arange Или linspace
+# b =np.arange(10)
+# print(b)
+# c = np.linspace(1,99,20).astype(int)
+# print(c)
+
+# После этого выполнить математические операции- сложить, перемножить, делить
+# Изменить форму массива
+
+# d1 = np.random.random_integers(1,99,size=(3,5))
+# d2 = np.linspace(1,99,15).astype(int)
+# d2 =d2.reshape((3,5))
+# print(d1)
+# print()
+# print(d2)
+# print("Суммирование:")
+# print(f"{d1+d2}")
+# print("Умножение:")
+# print(f"{d1*d2}")
+# print("Деление:")
+# print(f"{d1/d2}")
+
+
+# Найти максимальный элемент в каждой строчке и столбце первого массива (из случайных чисел_
+# print(a)
+# print()
+# print(a.min(axis=0))
+# print()
+# print(a.max(axis=1))
+
+
+# Найти максимальный элемент в из 2 массивов
+# mas1 = np.random.sample((2,2))
+# mas2 = np.random.sample((2,2))
+# print(mas1)
+# print()
+# print(mas2)
+# print()
+# print(f"{np.max((mas1,mas2))}")
+
+
